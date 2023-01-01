b9f4e483a65da08b5ff6077b65f72231ce23a8e2
UGI <sangugi01@gmail.com>
2023-01-01 11:00:26 +0900

    solved 120815

---
+++ Programmers/level0/120815.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+
+    for(let i = 1 ; i < 100 ; i++){
+
+        if(n*i%6 === 0){
+            answer = n*i/6;
+            break;
+        }
+    }
+
+    return answer;
+}
