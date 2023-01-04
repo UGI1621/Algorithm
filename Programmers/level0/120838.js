82dca37f17b4a8ca42e4087568d81bde9ad7d79c
UGI <sangugi01@gmail.com>
2023-01-04 09:03:59 +0900

    Create 120838.js

---
+++ Programmers/level0/120838.js
@@ -0,0 +1,16 @@
+morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+function solution(letter) {
+    let answer=[]
+    var letterList = letter.split(' ');
+    for(i of letterList){
+        answer.push(morse[i])
+    }
+    return answer.join('');
+}
