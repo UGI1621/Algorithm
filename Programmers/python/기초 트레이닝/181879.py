3fbfd44e6f5672b459a6b5939d0b2eca25de1091
UGI1621 <78088263+UGI1621@users.noreply.github.com>
2023-05-26 15:25:31 +0900

    Create 181879.py

---
+++ Programmers/python/기초 트레이닝/181879.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = 0
+    if len(num_list) > 10:
+        for i in num_list:
+            answer += i
+    else:
+        answer = 1
+        for i in num_list:
+            answer *= i
+
+    return answer
+
+# sum(num_list) <<
